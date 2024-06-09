b900878cc79f27f3ef681c790d0d269a1270c945
mehedihasanpiash <piash599@gmail.com>
2024-06-09 21:40:42 +0600

    - Update react native version 74.2

--- android/app/src/main/java/com/rnboilerplate/MainApplication.kt
+++ android/app/src/main/java/com/rnboilerplate/MainApplication.kt
@@ -3,15 +3,17 @@ package com.rnboilerplate;
 import android.app.Application
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
+import com.facebook.react.ReactHost
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
+import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
 
 class MainApplication : Application(), ReactApplication {
 
-  private val reactNativeHost: ReactNativeHost =
+    override val reactNativeHost: ReactNativeHost =
       object : DefaultReactNativeHost(this) {
         override fun getPackages(): List<ReactPackage> {
           // Packages that cannot be autolinked yet can be added manually here, for example:
@@ -24,15 +26,16 @@ class MainApplication : Application(), ReactApplication {
         override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
       }
 
-  override fun getReactNativeHost(): ReactNativeHost = reactNativeHost
+  override val reactHost: ReactHost
+      get() = getDefaultReactHost(applicationContext, reactNativeHost)
+
 
   override fun onCreate() {
-    super.onCreate()
-    SoLoader.init(this, false)
-    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-      // If you opted-in for the New Architecture, we load the native entry point for this app.
-      load()
-    }
-    ReactNativeFlipper.initializeFlipper(this, reactNativeHost.reactInstanceManager)
-  }
+      super.onCreate()
+      SoLoader.init(this, false)
+      if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+        // If you opted-in for the New Architecture, we load the native entry point for this app.
+        load()
+      }
+   }
 }
