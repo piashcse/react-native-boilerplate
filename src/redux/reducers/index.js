2039ec3cb7834b013d8cf6b8588ad6131eb739ad
Mehedi Hassan Piash <piash599@gmail.com>
2024-11-18 23:37:40 +0600

    - Configure ESLint

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+import networkTestReducer from './network-test';
+
+export default combineReducers({
+  networkTestReducer,
+});
---
+++ src/screens/Home.js
@@ -0,0 +1,38 @@
+import React, { useEffect } from 'react';
+import { ScrollView, StyleSheet, Text } from 'react-native';
+import { useSelector, useDispatch } from 'react-redux';
+import { getTestData } from '../redux/actions/network-demo';
+import Loading from '../components/loading/Loading';
+
+const Home = () => {
+  const dispatch = useDispatch();
+  const { isLoading, movies } = useSelector(
+    (state) => state?.networkTestReducer
+  );
+  useEffect(() => {
+    dispatch(getTestData());
+  }, []);
+
+  return isLoading ? (
+    <Loading />
+  ) : (
+    <ScrollView style={styles.container}>
+      <Text style={styles.welcome}>Welcome to React Native!</Text>
+      <Text>{JSON.stringify(movies, null, ' ')}</Text>
+    </ScrollView>
+  );
+};
+export default Home;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 16,
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+});
