27c7d062dcecf55b387c7cbc412bef9ff63c7c30
Mehedi Hassan Piash <piash599@gmail.com>
2025-03-14 20:45:30 +0600

    - Update boilerplate code with expo

--- src/appConstant/Colors.js
+++
@@ -1,9 +0,0 @@
-const colors = {
-  primaryColor: '#00ecd2',
-  transparentColor: 'transparent',
-  whiteColor: 'white',
-  black: 'black',
-  loadingBackgroundColor: 'rgba(0, 0, 0, 0.5)',
-};
-
-export { colors };
--- src/redux/sagas/index.js
+++
@@ -1,6 +0,0 @@
-import { all } from 'redux-saga/effects';
-import { networkTestSaga } from './network-test';
-
-export default function* rootSaga() {
-  yield all([...networkTestSaga]);
-}
