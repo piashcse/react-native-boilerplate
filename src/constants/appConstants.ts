df28c7566248494da2585116654827551d8aa3e3
Mehedi Hassan Piash <piash599@gmail.com>
2025-07-26 09:44:11 +0600

    - Update expo 53.0.19 to 53.0.20

--- src/constants/appConstants.ts
+++ src/constants/appConstants.ts
@@ -1,6 +1,6 @@
-const APP_CONSTANTS = {
+const AppConstants = {
   BASE_URL: 'https://api.themoviedb.org/3/',
   API_KEY: '59cd6896d8432f9c69aed9b86b9c2931',
 };
 
-export { APP_CONSTANTS };
+export { AppConstants };
